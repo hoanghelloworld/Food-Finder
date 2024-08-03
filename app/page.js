14a17bb304311d4a447ed7828814a9412c2548b5
Blns159 <22022632@vnu.edu.vn>
2024-08-03 09:27:39 +0700

    public data

--- app/page.js
+++ app/page.js
@@ -1,113 +1,107 @@
-import Image from "next/image";
+"use client"
+import { useContext, useEffect, useState } from 'react';
+// Định nghĩa sau:
+// import BusinessList from '@/components/Home/BusinessList';
+// import OSMMapView from '@/components/Home/OSMMapView';
+// import SkeltonLoading from '@/components/SkeltonLoading';
+import { UserLocationContext } from '@/context/UserLocationContext';
+import { signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import RangeSelect from '@/components/Home/RangeSelect';
+import SelectRating from '@/components/Home/SelectRating';
 
 export default function Home() {
+  const { data: session } = useSession();
+  const [radius, setRadius] = useState(10000); // Bán kính tìm kiếm
+  const [category, setCategory] = useState('Goi cuon');
+  const [businessList, setBusinessList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const { userLocation, setUserLocation } = useContext(UserLocationContext);
+  const [minRating, setMinRating] = useState(0);
+
+  // Push Login....
+  useEffect(() => {
+    if (!session?.user) {
+      router.push('/Login');
+    }
+  }, [session]);
+
+
+  // _______ VỊ TRÍ HIỆN TẠI ________
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        const roundedLatitude = Math.round(latitude * 10000000) / 10000000;
+        const roundedLongitude = Math.round(longitude * 10000000) / 10000000;
+        console.log(`Vị trí hiện tại:${roundedLatitude}, ${roundedLongitude}`);
+        setUserLocation({ lat: roundedLatitude, lng: roundedLongitude });
+      },
+      (error) => {
+        console.error("Error getting current location", error);
+      }
+    );
+  }, [setUserLocation]);
+  // _________________________________
+
+
+  // ________ LỌC QUÁN ĂN __________
+
+  useEffect(() => {
+    fetchData();
+  }, [category, radius, minRating, userLocation]);
+
+  const fetchData = () => {
+    if (category) {
+      setLoading(true);
+
+      fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+          const filteredData = data.locations.filter(location => {
+            const distance = calculateDistance(
+              location.lat,
+              location.lng,
+              userLocation.lat,
+              userLocation.lng
+            );
+            return location.label === category && distance <= radius / 1000 && location.totalScore >= minRating;
+          });
+
+          setBusinessList(filteredData);
+          console.log(businessList)
+          setLoading(false);
+        })
+        .catch(error => {
+          console.error('Error loading JSON:', error);
+          setLoading(false);
+        });
+    }
+  };
+  // ________________________________
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className='grid grid-cols-1 md:grid-cols-4 '>
+      <div className='p-3'>
+        <SelectRating onRatingChange={(value) => setMinRating(value)} />
+        <RangeSelect onRadiusChange={(value) => setRadius(value)} />
+      </div>
+      {/* BẢN ĐỒ */}
+      <div className='col-span-3'>
+        <OSMMapView businessList={businessList} />
+        <div className='md:absolute mx-2 w-[90%] md:w-[74%] bottom-36 relative md:bottom-3'>
+          {!loading ? <BusinessList businessList={businessList} />
+            :
+            <div className='flex gap-3'>
+              {[1, 2, 3, 4, 5].map((item, index) => (
+                <SkeltonLoading key={index} />
+              ))}
+            </div>
+          }
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
