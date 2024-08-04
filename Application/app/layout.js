9792a2eb5f60f1290d5541ec7da047bb54ffc616
Nguyễn Huy Hoàng <115699781+hoanghelloworld@users.noreply.github.com>
2024-08-04 01:12:55 +0000

    update chatbot

--- Application/app/layout.js
+++ Application/app/layout.js
@@ -6,7 +6,7 @@ import { Raleway } from 'next/font/google'
 import { useEffect, useState } from 'react'
 import { UserLocationContext } from '@/context/UserLocationContext'
 import { SelectedBusinessContext } from '@/context/SelectedBusinessContext'
-
+import ChatBot from '@/components/ChatBot'
 
 const raleway = Raleway({ subsets: ['latin'] })
 
@@ -41,6 +41,7 @@ export default function RootLayout({ children }) {
             <UserLocationContext.Provider value={{ userLocation, setUserLocation }}>
               {/* <HeaderNavBar /> */}
               {children}
+              <ChatBot />
             </UserLocationContext.Provider>
           </SelectedBusinessContext.Provider>
         </Provider>
